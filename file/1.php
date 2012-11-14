95f20bd15f2f49b0ca71e1f1aa2657474a2630d3
Wish <wishgelivable@gmail.com>
2012-11-14 10:17:44 +0800

    update 1.php

--- file/1.php
+++ file/1.php
@@ -8,7 +8,7 @@ test
 <!--获取html5表单元素传递过来的值：$_GET['name']-->
 <?php
 echo "你好";
-$text=$_GET['user_date'];
+$text=$_GET['usr_name'];
 echo $text."<br/>";
 echo "测试<br/>";
 echo "<input type='button' value='返回' onclick='history.back()'/>";
